e64e40062744d90d521ca4947608ec3331e85ac3
mikolaj.handzlik <miki.handzlikpl@gmail.com>
2024-02-28 11:07:45 +0100

    Nowe Metody w HOMECONTROLLER

--- Lab01/Controllers/HomeController.cs
+++ Lab01/Controllers/HomeController.cs
@@ -13,6 +13,42 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public string Search(string q, int counter)
+    {
+        return $"q={q}, counter{counter}";
+    }
+
+    [HttpPost]
+    public string Create(string name, int age)
+    {
+        return $"name={name}, age={age}";
+    }
+
+    [HttpGet]
+    
+    public Book FindBook(int id)
+    {
+       // Random random = new 
+       return new Book()
+       {
+           Id = id,
+           Title = "C#",
+           Pages = 234,
+       };
+    }
+    [HttpPost]
+    public Book CreateBook([FromBody] Book book)
+    {
+        Random random = new Random();
+        return new Book()
+        {
+            Id = random.Next(maxValue:100),
+            Title = book.Title,
+            Pages = book.Pages
+        };
+        
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -28,4 +64,12 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Pages { get; set; }
 }
